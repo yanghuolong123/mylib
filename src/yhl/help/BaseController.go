e60c4165644a80f814f27524d8323e5a30b05663
杨活龙 <yanghuolong@zhisland.com>
2017-11-06 10:42:12 +0800

    base controller

--- src/yhl/help/BaseController.go
+++ src/yhl/help/BaseController.go
@@ -23,18 +23,13 @@ func (this *BaseController) SendRes(code int, msg string, data interface{}) {
 
 func (this *BaseController) SendResJsonp(code int, msg string, data interface{}) {
 	m := make(map[string]interface{})
-	m["jsonrpc"] = "2.0"
-	m["success"] = true
-	if code != 0 {
-		m["success"] = false
-	}
 	m["code"] = code
 	m["msg"] = msg
 	if data != nil {
 		m["data"] = data
 	}
 
-	this.Data["json"] = m
-	this.ServeJSON()
+	this.Data["jsonp"] = m
+	this.ServeJSONP()
 	this.StopRun()
 }
