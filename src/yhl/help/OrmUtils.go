1d88ab41b2b15e5dd37520cfd72a378bfee1745b
杨活龙 <yanghuolong@zhisland.com>
2017-12-04 19:11:44 +0800

    orm utils

---
+++ src/yhl/help/OrmUtils.go
@@ -0,0 +1,19 @@
+package help
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	dbhost := beego.AppConfig.String("mysql.host")
+	dbport := beego.AppConfig.String("mysql.port")
+	dbuser := beego.AppConfig.String("mysql.user")
+	dbpasswd := beego.AppConfig.String("mysql.pass")
+	dbname := beego.AppConfig.String("mysql.dbname")
+
+	conn := dbuser + ":" + dbpasswd + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", conn)
+}
