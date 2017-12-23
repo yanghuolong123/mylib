6f5db488a98b6e6e2c732b6ff1c10840acfe4bf2
杨活龙 <yanghuolong@zhisland.com>
2017-12-23 17:33:08 +0800

    accesstoken log

--- src/yhl/wechat/Weixin.go
+++ src/yhl/wechat/Weixin.go
@@ -63,7 +63,7 @@ func GetAccessToken() (token string) {
 	b := httplib.Get(url)
 	data := make(map[string]interface{})
 	b.ToJSON(&data)
-	//fmt.Println(data)
+	fmt.Println(data)
 
 	if v, ok := data["access_token"]; ok {
 		token = v.(string)
