698de473c81758aa86fdddf6e143e27b1335d6a0
杨活龙 <yanghuolong@zhisland.com>
2018-02-01 20:16:26 +0800

    pay

--- src/yhl/wechat/wxpay/PayApi.go
+++ src/yhl/wechat/wxpay/PayApi.go
@@ -31,3 +31,16 @@ func UnifiedOrder(orderReq *UnifyOrderReq) UnifyOrderResp {
 	return res
 
 }
+
+func ToWxRespXmlStr(code, msg string) string {
+	resp := new(WXPayNotifyResp)
+	resp.Return_code = code
+	resp.Return_msg = msg
+
+	b, _ := xml.Marshal(resp)
+
+	strResp := strings.Replace(string(b), "WXPayNotifyResp", "xml", -1)
+	help.Log("wxpay", "strResp: "+strResp)
+
+	return strResp
+}
--- src/yhl/wechat/wxpay/PayData.go
+++ src/yhl/wechat/wxpay/PayData.go
@@ -66,3 +66,30 @@ func Sign(m map[string]interface{}) string {
 
 	return upperSign
 }
+
+type WXPayNotifyReq struct {
+	Return_code string `xml:"return_code"`
+	//	Return_msg     string `xml:"return_msg"`
+	Appid        string `xml:"appid"`
+	Mch_id       string `xml:"mch_id"`
+	Nonce        string `xml:"nonce_str"`
+	Sign         string `xml:"sign"`
+	Result_code  string `xml:"result_code"`
+	Openid       string `xml:"openid"`
+	Is_subscribe string `xml:"is_subscribe"`
+	Trade_type   string `xml:"trade_type"`
+	Bank_type    string `xml:"bank_type"`
+	Total_fee    int    `xml:"total_fee"`
+	Fee_type     string `xml:"fee_type"`
+	Cash_fee     int    `xml:"cash_fee"`
+	//	Cash_fee_Type  string `xml:"cash_fee_type"`
+	Transaction_id string `xml:"transaction_id"`
+	Out_trade_no   string `xml:"out_trade_no"`
+	//	Attach         string `xml:"attach"`
+	Time_end string `xml:"time_end"`
+}
+
+type WXPayNotifyResp struct {
+	Return_code string `xml:"return_code"`
+	Return_msg  string `xml:"return_msg"`
+}
