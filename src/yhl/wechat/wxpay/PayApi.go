a168105d3155684cc901944115bd8af968280795
杨活龙 <yanghuolong@zhisland.com>
2018-02-02 15:38:36 +0800

    pay

--- src/yhl/wechat/wxpay/PayApi.go
+++ src/yhl/wechat/wxpay/PayApi.go
@@ -2,7 +2,6 @@ package wxpay
 
 import (
 	"encoding/xml"
-	"fmt"
 	"github.com/astaxie/beego/httplib"
 	"yhl/help"
 )
@@ -13,13 +12,11 @@ func UnifiedOrder(orderReq *UnifyOrderReq) UnifyOrderResp {
 	orderReq.Nonce_str = help.RandStr(32)
 
 	m := help.StructToMap(*orderReq)
-	fmt.Printf("m:%+v", m)
 
 	orderReq.Sign = Sign(m)
 
 	xmlByte, _ := xml.MarshalIndent(orderReq, "", "    ")
 	xmlStr := string(xmlByte)
-	fmt.Println("========== xmlStr:", xmlStr)
 
 	url := "https://api.mch.weixin.qq.com/pay/unifiedorder"
 	req := httplib.Post(url)
