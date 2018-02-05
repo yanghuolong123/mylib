acde16dd4b9a4a455a947b9881c6b85e70b19cb6
杨活龙 <yanghuolong@zhisland.com>
2018-02-05 19:49:19 +0800

    pay

--- src/yhl/wechat/wxpay/PayApi.go
+++ src/yhl/wechat/wxpay/PayApi.go
@@ -2,7 +2,10 @@ package wxpay
 
 import (
 	"encoding/xml"
+	"github.com/astaxie/beego/context"
 	"github.com/astaxie/beego/httplib"
+	"net/url"
+	"time"
 	"yhl/help"
 )
 
@@ -18,8 +21,8 @@ func UnifiedOrder(orderReq *UnifyOrderReq) UnifyOrderResp {
 	xmlByte, _ := xml.MarshalIndent(orderReq, "", "    ")
 	xmlStr := string(xmlByte)
 
-	url := "https://api.mch.weixin.qq.com/pay/unifiedorder"
-	req := httplib.Post(url)
+	urlStr := "https://api.mch.weixin.qq.com/pay/unifiedorder"
+	req := httplib.Post(urlStr)
 	req.Header("Accept", "application/xml")
 	req.Header("Content-Type", "application/xml;charset=utf-8")
 	req.Body(xmlStr)
@@ -27,5 +30,44 @@ func UnifiedOrder(orderReq *UnifyOrderReq) UnifyOrderResp {
 	req.ToXML(&res)
 
 	return res
+
+}
+
+func JsPaySdk(prepayId string) map[string]interface{} {
+	m := map[string]interface{}{}
+
+	m["appId"] = AppId
+	m["timeStamp"] = time.Now().Unix()
+	m["nonceStr"] = help.RandStr(32)
+	m["package"] = "prepay_id=" + prepayId
+	m["signType"] = "MD5"
+	sign := Sign(m)
+	m["paySign"] = sign
+
+	return m
+}
+
+func GetOpenId(c *context.Context, urlStr string) (openid string) {
+	urlStr = url.QueryEscape(urlStr)
+	if code := c.Input.Query("code"); code == "" {
+
+		codeUrl := "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + AppId + "&redirect_uri=" + urlStr + "&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect"
+
+		c.Redirect(302, codeUrl)
+
+	} else {
+		openidUrl := "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + AppId + "&secret=" + AppSecret + "&code=" + code + "&grant_type=authorization_code"
+
+		b := httplib.Get(openidUrl)
+		data := make(map[string]interface{})
+		b.ToJSON(&data)
+
+		help.Log("wxpay", data)
+		if v, ok := data["openid"]; ok {
+			openid = v.(string)
+		}
+	}
+
+	return
 
 }
