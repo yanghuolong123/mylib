dbcb8fcdec32e5963c085389bbf89d0923a4016b
杨活龙 <yanghuolong@zhisland.com>
2018-03-08 18:04:40 +0800

    api

---
+++ src/yhl/api/UploadController.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"fmt"
+	"path/filepath"
+	"time"
+	"yhl/help"
+)
+
+type UploadController struct {
+	help.BaseController
+}
+
+func (this *UploadController) Uploadfile() {
+	f, h, err := this.GetFile("file")
+	if err != nil {
+		this.SendRes(-1, err.Error(), nil)
+	}
+	defer f.Close()
+
+	ext := filepath.Ext(h.Filename)
+	filename := time.Now().Format(help.DatetimeNumFormat) + help.IntToString(help.RandNum(1000, 9999)) + ext
+	dir := "uploads/"
+	y, m, d := help.Date()
+	dir += fmt.Sprintf("%d/%d/%d/", y, m, d)
+	help.MkDirPath(dir)
+	fileRoute := dir + filename
+	this.SaveToFile("file", fileRoute)
+
+	this.SendRes(0, "success", fileRoute)
+}
