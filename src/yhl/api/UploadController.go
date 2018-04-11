8fc09e85416b568c58d4c2b5b93cc32f8929c0d3
杨活龙 <yanghuolong@zhisland.com>
2018-04-11 17:30:11 +0800

    ckeditor upload

--- src/yhl/api/UploadController.go
+++ src/yhl/api/UploadController.go
@@ -29,3 +29,28 @@ func (this *UploadController) Uploadfile() {
 
 	this.SendRes(0, "success", "/"+fileRoute)
 }
+
+func (this *UploadController) CkUploadfile() {
+	callback := this.GetString("CKEditorFuncNum")
+	f, h, err := this.GetFile("upload")
+	if err != nil {
+		this.SendRes(-1, err.Error(), nil)
+	}
+	defer f.Close()
+
+	ext := filepath.Ext(h.Filename)
+	filename := time.Now().Format(help.DatetimeNumFormat) + help.IntToStr(help.RandNum(1000, 9999)) + ext
+	dir := "uploads/"
+	y, m, d := help.Date()
+	dir += fmt.Sprintf("%d/%d/%d/", y, m, d)
+	help.MkDirPath(dir)
+	fileRoute := dir + filename
+	this.SaveToFile("upload", fileRoute)
+	fpath := "/" + fileRoute
+
+	out := "<script type=\"text/javascript\">"
+	out += "window.parent.CKEDITOR.tools.callFunction(" + callback + ",\"" + fpath + "\",\"\")"
+	out += "</script>"
+
+	this.Ctx.WriteString(out)
+}
