45e926fcef014c138cb2e913de671fca41b360f7
杨活龙 <yanghuolong@zhisland.com>
2018-08-21 13:52:54 +0800

    convert to utf8

--- src/yhl/help/StringUtils.go
+++ src/yhl/help/StringUtils.go
@@ -3,6 +3,7 @@ package help
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/axgle/mahonia"
 	"strconv"
 )
 
@@ -23,4 +24,13 @@ func ToStr(v interface{}) string {
 
 func SubStr(str string, start, length int) string {
 	return beego.Substr(str, start, length)
+}
+
+func ConvertToString(src string, srcCode string, tagCode string) string {
+	srcCoder := mahonia.NewDecoder(srcCode)
+	srcResult := srcCoder.ConvertString(src)
+	tagCoder := mahonia.NewDecoder(tagCode)
+	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
+	result := string(cdata)
+	return result
 }
